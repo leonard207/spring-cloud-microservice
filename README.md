5b51e58fe63737b4d9ff7d4d698971e96e4b47b3
Leonard <leonard.li@rbyair.com>
2016-07-06 16:04:02 +0800

    服务发现、配置模块提交

---
+++ README.md
@@ -0,0 +1,10 @@
+# spring-cloud-microservice
+
+## 项目说明
+
+这是一个微服务项目例子，使用spring-cloud搭建的工程基础环境.
+
+## 开发环境
+
+* Java 1.8
+* Gradle 2.x
---
+++ discovery-service/src/main/java/com/leonard/discovery/DiscoveryApplication.java
@@ -0,0 +1,18 @@
+package com.leonard.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Leonard on 2016/7/6.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+
+}
