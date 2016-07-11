448fb1d3a198d5bc8c86d63488ce2755b91490f0
Leonard <leonard.li@rbyair.com>
2016-07-11 13:25:25 +0800

    添加Hystrix Dashboard

---
+++ hystrix-dashboard/src/main/java/com/leonard/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.leonard.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by Leonard on 2016/7/7.
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
