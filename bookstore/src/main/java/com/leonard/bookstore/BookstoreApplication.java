213ce49cef13e80a833153115b99dda3f54a3676
Leonard <941668690@qq.com>
2016-11-17 14:53:11 +0800

    config-service 更新

--- bookstore/src/main/java/com/leonard/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/leonard/bookstore/BookstoreApplication.java
@@ -1,14 +1,14 @@
 package com.leonard.bookstore;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Leonard on 2016/7/7.
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 public class BookstoreApplication {
 
     public static void main(String[] args) {
